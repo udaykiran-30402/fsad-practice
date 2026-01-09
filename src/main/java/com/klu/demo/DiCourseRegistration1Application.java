8bf741a5348b46d3b8db2c0b9bb826dfa11af1ae
udaykiran-30402 <2400030402@kluniversity.in>
2026-01-09 08:00:48 +0530

    Spring DI Course Registration using Constructor and Setter Injection

---
+++ src/main/java/com/klu/demo/DiCourseRegistration1Application.java
@@ -0,0 +1,13 @@
+package com.klu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiCourseRegistration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiCourseRegistration1Application.class, args);
+	}
+
+}
